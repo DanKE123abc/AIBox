ebcf076000baecf96c524d418242aae48538ba6c
DanKe <1586017466@qq.com>
2023-06-28 15:10:56 +0800

    第一次提交

---
+++ ChatApi/ChatApi.cs
@@ -0,0 +1,62 @@
+﻿using LitJson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIBox.ChatApi
+{
+    public class ChatApi
+    {
+        private const string api = "https://fd.52ai.pw/v1/chat/completions";
+        private const string model = "gpt-3.5-turbo";
+        private const string api_key = "Bearer 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";
+        List<Dictionary<string, string>> messages = new List<Dictionary<string, string>>
+    {
+        new Dictionary<string, string>() { { "role", "user" }, { "content", "" } }
+    };
+
+        private string AskChatGpt(List<Dictionary<string, string>> messages)
+        {
+            var data = new Dictionary<string, object>
+        {
+            { "model", model },
+            { "messages", messages },
+            { "stream", false }
+        };
+            var json = JsonMapper.ToJson(data);
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", api_key);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = client.PostAsync(api, content).Result;
+            var responseJson = response.Content.ReadAsStringAsync().Result;
+
+            //Debug.Log(responseJson);
+            var responseDict = JsonMapper.ToObject(responseJson);
+            return responseDict["choices"][0]["message"]["content"].ToString();
+        }
+
+        public string Get(string content)
+        {
+            try
+            {
+                var d = new Dictionary<string, string>() { { "role", "user" }, { "content", content } };
+                messages.Add(d);
+
+                content = AskChatGpt(messages);
+
+                d = new Dictionary<string, string>() { { "role", "assistant" }, { "content", content } };
+                messages.Add(d);
+                return "" + content;
+            }
+            catch (Exception ex)
+            {
+                messages.RemoveAt(messages.Count - 1);
+                return "出现错误，请联系开发者" + ex;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using LitJson;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+using AIBox.ChatApi;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace AIBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // 在 MainWindow 类中添加以下变量
+        private int badgeCount;
+
+        private ObservableCollection<MessageItem> conversation;
+        private ChatApi.ChatApi api = new ChatApi.ChatApi();
+
+        public class MessageItem
+        {
+            public string UserMessage { get; set; }
+            public string AgentMessage { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            badgeCount = 0;
+
+            // 初始化对话集合
+            conversation = new ObservableCollection<MessageItem>();
+            ConversationListBox.ItemsSource = conversation;
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; // 最小化窗口
+        }
+
+        private void Off_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("你确定要关闭AI助手吗？", "你按下了关闭按钮", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.OK)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void RestartApplication_Click(object sender, RoutedEventArgs e)
+        {
+            api = null;
+            api = new ChatApi.ChatApi();
+            conversation.Clear();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 获取屏幕的宽度和高度
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            // 获取任务栏的高度
+            double taskbarHeight = SystemParameters.PrimaryScreenHeight - SystemParameters.WorkArea.Height;
+
+            // 设置窗口的高度为屏幕的高度减去任务栏高度
+            this.Height = screenHeight - taskbarHeight;
+
+            // 设置窗口的位置
+            this.Left = screenWidth - this.Width;
+            this.Top = (screenHeight - this.Height) / 2;
+
+            this.WindowStyle = WindowStyle.None;
+            DisableWindowDragging(this);
+            this.Topmost = true; // 设置窗口始终显示在顶部
+            this.Top = 0; // 设置窗口纵坐标为0，将窗口放置在屏幕的顶部
+            this.Topmost = true; // 设置窗口始终显示在顶部
+        }
+
+        private void DisableWindowDragging(Window window)
+        {
+            IntPtr hwnd = new WindowInteropHelper(window).Handle;
+            HwndSource.FromHwnd(hwnd)?.AddHook(WindowProc);
+        }
+
+        private static IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_MOVE = 0xF010;
+
+            if (msg == WM_SYSCOMMAND && (int)wParam == SC_MOVE)
+            {
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            Send();
+        }
+
+        private void Send()
+        {
+            string userMessage = InputTextBox.Text;
+            if (!string.IsNullOrEmpty(userMessage))
+            {
+
+                // 模拟对方回复
+                string agentMessage = api.Get(userMessage);
+
+                /// 创建消息项，包含用户对话和对方回复
+                MessageItem messageItem = new MessageItem
+                {
+                    UserMessage = userMessage,
+                    AgentMessage = agentMessage
+                };
+
+                // 添加消息项到对话集合
+                conversation.Add(messageItem);
+
+
+                // 清空输入框
+                InputTextBox.Text = "";
+
+                // 滚动到最新的对话
+                ConversationListBox.ScrollIntoView(conversation.LastOrDefault());
+
+                // 修改焦点以便于继续输入
+                InputTextBox.Focus();
+            }
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.S)
+            {
+                if(this.WindowState == WindowState.Minimized)
+                {
+                    this.WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Minimized;
+                }
+                
+            }
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.Enter)
+            {
+                Send();
+            }
+        }
+    }
+
+
+}
